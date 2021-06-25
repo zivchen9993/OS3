5923711c5fa74a5247ecadf6622967ea3c6b8810
Ziv Chen <ziv.chen@campus.technion.ac.il>
2021-06-25 17:57:39 +0300

    add tests

---
+++ tests/try_first.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+nc -w0 -u 127.0.0.1 20000 < wrq_packet
+nc -w0 -u 127.0.0.1 20000 < data1
+sleep 5
+#nc -w0 -u 127.0.0.1 20000 < data2
+nc -w0 -u 127.0.0.1 20000 < data3
+nc -w0 -u 127.0.0.1 20000 < data4
+nc -w0 -u 127.0.0.1 20000 < data5
+nc -w0 -u 127.0.0.1 20000 < data6
+nc -w0 -u 127.0.0.1 20000 < data7
+nc -w0 -u 127.0.0.1 20000 < data8
+nc -w0 -u 127.0.0.1 20000 < data9
+nc -w0 -u 127.0.0.1 20000 < data10
+nc -w0 -u 127.0.0.1 20000 < data11
+nc -w0 -u 127.0.0.1 20000 < data12
+nc -w0 -u 127.0.0.1 20000 < data13
+nc -w0 -u 127.0.0.1 20000 < data14
+
+#sleep 5
+nc -w0 -u 127.0.0.1 20000 < data15
+#sleep 5
+nc -w0 -u 127.0.0.1 20000 < data16
+#sleep 5
+nc -w0 -u 127.0.0.1 20000 < data17
